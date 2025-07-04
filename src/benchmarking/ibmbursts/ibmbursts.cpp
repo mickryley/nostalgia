3096d26cfb1bb63a77a526efe643beccf2d8cff9
mickryley <108213217+mickryley@users.noreply.github.com>
2025-07-04 15:21:13 +0900

    Inl IBM Applied

--- src/benchmarking/ibmbursts/ibmbursts.cpp
+++ src/benchmarking/ibmbursts/ibmbursts.cpp
@@ -89,162 +89,21 @@ namespace nostalgia::benchmarking::IBMBursts {
 		void run_ibmbursts_benchmark(int iterations, int passes, int size){}
 	}
 	namespace pool {
-#pragma region Allocation Specific Macros - These must be set for every namespace 
-#define NAME_ALLOCATOR		"Pool Allocator"
-#define OBJECT_LOCAL_OVERRIDE_STATIC_ACCESS	nostalgia::pool::objects::Vector2D_LocalOverride_StaticAccess
-#pragma endregion
+	#define NAME_ALLOCATOR		"Pool Allocator"
+	#define OBJECT_LOCAL_OVERRIDE_STATIC_ACCESS	nostalgia::pool::objects::Vector2D_LocalOverride_StaticAccess
 
-		void run_static_v2d_contPointer(int iterations, int passes, int size) {
+	#include "benchmarking/ibmbursts/ibmbursts.inl"
 
-			IMPLEMENTATION_DETAILS("Static V2D Cont Pointer",
-				std::format("{}x ({}x alloc + {}x free)", passes, iterations, iterations).c_str());
-			BEGIN_ALL_TIMERS();
-
-			for (size_t i = 0; i < passes; i++)
-			{
-				START_ALLOC_TIMERS();
-
-				auto** vec = new OBJECT_LOCAL_OVERRIDE_STATIC_ACCESS * [iterations]; // [ALLOC SPECIFIC]
-
-				for (size_t j = 0; j < iterations; j++) {
-					vec[j] = new OBJECT_LOCAL_OVERRIDE_STATIC_ACCESS(1.0f, 2.0f);    // [ALLOC SPECIFIC]
-				}
-				PAUSE_ALLOC_TIMERS();
-
-				START_DEALLOC_TIMERS();
-
-				for (int k = static_cast<int>(iterations) - 1; k >= 0; --k) {
-					delete vec[k];
-				}
-
-				delete[] vec;
-
-				PAUSE_DEALLOC_TIMERS();
-			}
-
-			STOP_ALL_TIMERS();
-			PRINT_ALL_TIMERS();
-			EXPORT_BENCHMARK_RESULTS();
-		}
-
-		void run_static_v2d_contPointer_forwardDealloc(int iterations, int passes, int size) {
-
-			IMPLEMENTATION_DETAILS("Static V2D Cont Pointer Forward Dealloc", 
-				std::format("{}x ({}x alloc + {}x free)", passes, iterations, iterations).c_str());
-			BEGIN_ALL_TIMERS();
-
-			for (size_t i = 0; i < passes; i++)
-			{
-				START_ALLOC_TIMERS();
-
-				auto** vec = new OBJECT_LOCAL_OVERRIDE_STATIC_ACCESS * [iterations]; // [ALLOC SPECIFIC]
-
-				for (size_t j = 0; j < iterations; j++) {
-					vec[j] = new OBJECT_LOCAL_OVERRIDE_STATIC_ACCESS(1.0f, 2.0f);    // [ALLOC SPECIFIC]
-				}
-				PAUSE_ALLOC_TIMERS();
-
-				START_DEALLOC_TIMERS();
-
-				for (size_t k = 0; k < iterations; k++) {
-					delete vec[k];
-				}
-
-				delete[] vec;
-
-				PAUSE_DEALLOC_TIMERS();
-			}
-
-			STOP_ALL_TIMERS();
-			PRINT_ALL_TIMERS();
-			EXPORT_BENCHMARK_RESULTS();			
-		}
-
-		void run_ibmbursts_benchmark(int iterations, int passes, int size) {
-			run_static_v2d_contPointer(iterations, passes, size);
-			run_static_v2d_contPointer_forwardDealloc(iterations, passes, size);
-		}
-#undef OBJECT_LOCAL_OVERRIDE_STATIC_ACCESS
+	#undef OBJECT_LOCAL_OVERRIDE_STATIC_ACCESS
+	#undef NAME_ALLOCATOR
 	}
 	namespace freelist {
-#pragma region Allocation Specific Macros - These must be set for every namespace 
-#define NAME_ALLOCATOR		"Freelist Allocator"
-#define OBJECT_LOCAL_OVERRIDE_STATIC_ACCESS	nostalgia::freelist::objects::Vector2D_LocalOverride_StaticAccess
-#pragma endregion
+	#define NAME_ALLOCATOR		"Freelist Allocator"
+	#define OBJECT_LOCAL_OVERRIDE_STATIC_ACCESS	nostalgia::freelist::objects::Vector2D_LocalOverride_StaticAccess
+		
+	#include "benchmarking/ibmbursts/ibmbursts.inl"
 
-		void run_static_v2d_contPointer(int iterations, int passes, int size) {
-			
-			
-			IMPLEMENTATION_DETAILS("Static V2D Cont Pointer",
-				std::format("{}x ({}x alloc + {}x free)", passes, iterations, iterations).c_str());
-			BEGIN_ALL_TIMERS();
-
-			for (size_t i = 0; i < passes; i++)
-			{
-				START_ALLOC_TIMERS();
-
-				auto** vec = new OBJECT_LOCAL_OVERRIDE_STATIC_ACCESS * [iterations]; // [ALLOC SPECIFIC]
-
-				for (size_t j = 0; j < iterations; j++) {
-					vec[j] = new OBJECT_LOCAL_OVERRIDE_STATIC_ACCESS(1.0f, 2.0f);    // [ALLOC SPECIFIC]
-				}
-				PAUSE_ALLOC_TIMERS();
-
-				START_DEALLOC_TIMERS();
-
-				for (int k = static_cast<int>(iterations) - 1; k >= 0; --k) {
-					delete vec[k];
-				}
-
-				delete[] vec;
-
-				PAUSE_DEALLOC_TIMERS();
-			}
-
-			STOP_ALL_TIMERS();
-			PRINT_ALL_TIMERS();
-			EXPORT_BENCHMARK_RESULTS();
-		}
-
-		void run_static_v2d_contPointer_forwardDealloc(int iterations, int passes, int size) {
-
-			IMPLEMENTATION_DETAILS("Static V2D Cont Pointer Forward Dealloc",
-				std::format("{}x ({}x alloc + {}x free)", passes, iterations, iterations).c_str());
-			BEGIN_ALL_TIMERS();
-
-			for (size_t i = 0; i < passes; i++)
-			{
-				START_ALLOC_TIMERS();
-
-				auto** vec = new OBJECT_LOCAL_OVERRIDE_STATIC_ACCESS * [iterations]; // [ALLOC SPECIFIC]
-
-				for (size_t j = 0; j < iterations; j++) {
-					vec[j] = new OBJECT_LOCAL_OVERRIDE_STATIC_ACCESS(1.0f, 2.0f);    // [ALLOC SPECIFIC]
-				}
-				PAUSE_ALLOC_TIMERS();
-
-				START_DEALLOC_TIMERS();
-
-				for (size_t k = 0; k < iterations; k++) {
-					delete vec[k];
-				}
-
-				delete[] vec;
-
-				PAUSE_DEALLOC_TIMERS();
-			}
-
-			STOP_ALL_TIMERS();
-			PRINT_ALL_TIMERS();
-			EXPORT_BENCHMARK_RESULTS();
-		}
-
-
-		void run_ibmbursts_benchmark(int iterations, int passes, int size) {
-			run_static_v2d_contPointer(iterations, passes, size);
-			run_static_v2d_contPointer_forwardDealloc(iterations, passes, size);
-		}
-
-#undef OBJECT_LOCAL_OVERRIDE_STATIC_ACCESS
+	#undef OBJECT_LOCAL_OVERRIDE_STATIC_ACCESS
+	#undef NAME_ALLOCATOR
 	}
 }
--- src/benchmarking/ibmbursts/ibmbursts.inl
+++ src/benchmarking/ibmbursts/ibmbursts.inl
@@ -1,67 +1,72 @@
-//#include "benchmarking/macros.h" // Ensure is commented out in the final build
+//#include "benchmarking/macros.h" // Ensure is commented out at compile time
 
 void run_static_v2d_contPointer(int iterations, int passes, int size) {
-						
-			IMPLEMENTATION_DETAILS("Static V2D Cont Pointer",
-				std::format("{}x ({}x alloc + {}x free)", passes, iterations, iterations).c_str());
-			BEGIN_ALL_TIMERS();
 
-			for (size_t i = 0; i < passes; i++)
-			{
-				START_ALLOC_TIMERS();
+	IMPLEMENTATION_DETAILS("Static V2D Cont Pointer",
+		std::format("{}x ({}x alloc + {}x free)", passes, iterations, iterations).c_str());
+	BEGIN_ALL_TIMERS();
 
-				auto** vec = new OBJECT_LOCAL_OVERRIDE_STATIC_ACCESS * [iterations]; // [ALLOC SPECIFIC]
+	for (size_t i = 0; i < passes; i++)
+	{
+		START_ALLOC_TIMERS();
 
-				for (size_t j = 0; j < iterations; j++) {
-					vec[j] = new OBJECT_LOCAL_OVERRIDE_STATIC_ACCESS(1.0f, 2.0f);    // [ALLOC SPECIFIC]
-				}
-				PAUSE_ALLOC_TIMERS();
+		auto** vec = new OBJECT_LOCAL_OVERRIDE_STATIC_ACCESS * [iterations]; // [ALLOC SPECIFIC]
 
-				START_DEALLOC_TIMERS();
+		for (size_t j = 0; j < iterations; j++) {
+			vec[j] = new OBJECT_LOCAL_OVERRIDE_STATIC_ACCESS(1.0f, 2.0f);    // [ALLOC SPECIFIC]
+		}
+		PAUSE_ALLOC_TIMERS();
 
-				for (int k = static_cast<int>(iterations) - 1; k >= 0; --k) {
-					delete vec[k];
-				}
+		START_DEALLOC_TIMERS();
 
-				delete[] vec;
-
-				PAUSE_DEALLOC_TIMERS();
-			}
-
-			STOP_ALL_TIMERS();
-			PRINT_ALL_TIMERS();
-			EXPORT_BENCHMARK_RESULTS();
+		for (int k = static_cast<int>(iterations) - 1; k >= 0; --k) {
+			delete vec[k];
 		}
 
-		void run_static_v2d_contPointer_forwardDealloc(int iterations, int passes, int size) {
+		delete[] vec;
 
-			IMPLEMENTATION_DETAILS("Static V2D Cont Pointer Forward Dealloc",
-				std::format("{}x ({}x alloc + {}x free)", passes, iterations, iterations).c_str());
-			BEGIN_ALL_TIMERS();
+		PAUSE_DEALLOC_TIMERS();
+	}
 
-			for (size_t i = 0; i < passes; i++)
-			{
-				START_ALLOC_TIMERS();
+	STOP_ALL_TIMERS();
+	PRINT_ALL_TIMERS();
+	EXPORT_BENCHMARK_RESULTS();
+}
 
-				auto** vec = new OBJECT_LOCAL_OVERRIDE_STATIC_ACCESS * [iterations]; // [ALLOC SPECIFIC]
+void run_static_v2d_contPointer_forwardDealloc(int iterations, int passes, int size) {
 
-				for (size_t j = 0; j < iterations; j++) {
-					vec[j] = new OBJECT_LOCAL_OVERRIDE_STATIC_ACCESS(1.0f, 2.0f);    // [ALLOC SPECIFIC]
-				}
-				PAUSE_ALLOC_TIMERS();
+	IMPLEMENTATION_DETAILS("Static V2D Cont Pointer Forward Dealloc",
+		std::format("{}x ({}x alloc + {}x free)", passes, iterations, iterations).c_str());
+	BEGIN_ALL_TIMERS();
 
-				START_DEALLOC_TIMERS();
+	for (size_t i = 0; i < passes; i++)
+	{
+		START_ALLOC_TIMERS();
 
-				for (size_t k = 0; k < iterations; k++) {
-					delete vec[k];
-				}
+		auto** vec = new OBJECT_LOCAL_OVERRIDE_STATIC_ACCESS * [iterations]; // [ALLOC SPECIFIC]
 
-				delete[] vec;
-
-				PAUSE_DEALLOC_TIMERS();
-			}
-
-			STOP_ALL_TIMERS();
-			PRINT_ALL_TIMERS();
-			EXPORT_BENCHMARK_RESULTS();
+		for (size_t j = 0; j < iterations; j++) {
+			vec[j] = new OBJECT_LOCAL_OVERRIDE_STATIC_ACCESS(1.0f, 2.0f);    // [ALLOC SPECIFIC]
 		}
+		PAUSE_ALLOC_TIMERS();
+
+		START_DEALLOC_TIMERS();
+
+		for (size_t k = 0; k < iterations; k++) {
+			delete vec[k];
+		}
+
+		delete[] vec;
+
+		PAUSE_DEALLOC_TIMERS();
+	}
+
+	STOP_ALL_TIMERS();
+	PRINT_ALL_TIMERS();
+	EXPORT_BENCHMARK_RESULTS();
+}
+
+void run_ibmbursts_benchmark(int iterations, int passes, int size) {
+	run_static_v2d_contPointer(iterations, passes, size);
+	run_static_v2d_contPointer_forwardDealloc(iterations, passes, size);
+}
